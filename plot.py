c2b9f8af41b91e357c77c95eea571865ad0e74fb
kaushal.agarwal2016@vitstudent.ac.in <kaushalagarwal79@gmail.com>
2018-10-28 16:20:47 +0530

    Bar graphs of months vs cummulative sum of subjectivity & polarity

--- plot.py
+++ plot.py
@@ -4,6 +4,7 @@ from opinionAnalysis import polarity
 import nltk
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 data = {}
 
@@ -17,13 +18,24 @@ grouped = df.groupby('Month')
 
 print("Subjectivity")
 print()
-
-print(grouped['Subjectivity'].agg([np.sum, np.mean, np.std]))
+print(grouped['Subjectivity'].agg([np.mean, np.std]))
 print()
 print("Polarity")
 print()
+print(grouped['Polarity'].agg([np.mean, np.std]))
 
-print(grouped['Polarity'].agg([np.sum, np.mean, np.std]))
+plt.figure(1)
+plot_data = df.groupby('Month')['Subjectivity'].sum()
+plot_data.sort_values().plot(kind='bar')
+plt.title("Months")
+plt.ylabel("Subjectivity")
 
+plt.figure(2)
+plot_data = df.groupby('Month')['Polarity'].sum()
+plot_data.sort_values().plot(kind='bar')
+plt.title("Months")
+plt.ylabel("Polarity")
+
+plt.figure(3)
 freq = nltk.FreqDist(upload)
 freq.plot(cumulative=False)
--- retrieveReviews.py
+++ retrieveReviews.py
@@ -2,26 +2,32 @@ from retriveColleges import colleges
 from selenium import webdriver
 
 def getReviews(url):
+
     driver.get(url)
     elem = driver.find_elements_by_class_name("rvwv1Heading")
     reviews = driver.find_elements_by_xpath("//p[strong = 'Placements :']")
-    if(len(elem)>0):
+
+    if elem:
+
         for i in range(0, len(elem)):
+
             user = str(elem[i].find_element_by_css_selector("p").text)
             p = user.find(',')
             name = user[3:p]
             strings = user.split()
-            month = strings[3]
+            months = ["Jan","Feb","Mar","Apr","May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-            if month == "Jan" or month == "Feb" or month == "Mar" or month == "Apr" or month == "May" or month == "Jun" or month == "Jul" or month == "Aug" or month == "Sep" or month == "Oct" or month == "Nov" or month == "Dec":
-                if user:
-                    if reviews[i]:
-                        review = reviews[i].text[13:]
-                        row = []
-                        row.append(name)
-                        row.append(month)
-                        row.append(review)
-                        matrix.append(row)
+            for k in range(0, len(strings)):
+                if strings[k] in months:
+                    month = strings[k]
+                    break
+
+            review = reviews[i].text[13:]
+            row = []
+            row.append(name)
+            row.append(month)
+            row.append(review)
+            matrix.append(row)
 
 matrix = [[]]
 
